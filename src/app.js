d9e54b3bb7be7dd77397778fd48bcede9e5768ea
phup2555 <xxphupxx25@gmail.com>
2026-01-14 23:44:12 +0700

    fix allogs

--- src/app.js
+++ src/app.js
@@ -5,16 +5,15 @@ import { connectDB } from "./config/db.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 import http from "http";
 import { Server } from "socket.io";
-
+import { sql } from "./config/db.js";
 const app = express();
 const server = http.createServer(app);
 
 const pool = await connectDB();
 
-// 🔹 CORS Configuration
 const whitelist = [
-  "https://lgstorageservice.com",
-  "https://www.lgstorageservice.com",
+  "https://sboxlaos.com",
+  "https://www.sboxlaos.com",
   "http://localhost:5173",
 ];
 
@@ -35,7 +34,63 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(express.json());
+app.use((req, res, next) => {
+  const originalSend = res.send;
 
+  res.send = async function (body) {
+    res.send = originalSend;
+    res.send(body);
+
+    if (req.method !== "GET") {
+      console.log("method", req.method);
+      console.log("req.headers", req.headers);
+      console.log("req.originalUrl", req.originalUrl);
+      console.log("res.statusCode", res.statusCode);
+      console.log("req.body", req.body);
+      console.log("req.params", req.params);
+      console.log("response body", body);
+      try {
+        const pool = await connectDB();
+        await pool
+          .request()
+          .input("headers", JSON.stringify(req.headers))
+          .input("method", req.method)
+          .input("url", req.originalUrl)
+          .input("statusCode", res.statusCode)
+          .input(
+            "request",
+            sql.NVarChar(sql.MAX),
+            JSON.stringify(req.body ?? req.params)
+          )
+          .input("response", sql.NVarChar(sql.MAX), JSON.stringify(body))
+          .query(`
+            INSERT INTO allLogs (
+              headers,
+              method,
+              url,
+              statusCode,
+              request,
+              response,
+              created_at
+            )
+            VALUES (
+              @headers,
+              @method,
+              @url,
+              @statusCode,
+              @request,
+              @response,
+              GETDATE()
+            )
+          `);
+      } catch (err) {
+        console.error("Log save error:", err);
+      }
+    }
+  };
+
+  next();
+});
 // 🔹 Socket.IO
 export const io = new Server(server, {
   cors: {
